8e6711b7c3da848c542c93b954ee0c9555464168
Camilli Joannes <joannescamilli1@gmail.com>
2022-10-18 19:26:57 -0300

    Adding the Search Function

--- src/RpgDemonSlayer.jsx
+++ src/RpgDemonSlayer.jsx
@@ -8,6 +8,7 @@ import muzan from "./assets/muzan.png"
 
 function RpgDemonSlayer() {
 
+
   const [habilidade, setHabilidade] = useState([
     {
       forma: "1° Forma",
@@ -32,7 +33,9 @@ function RpgDemonSlayer() {
   function showModal() {
     document.getElementById("modal").style.display = 'block';
   }
-
+  function showModalBusca() {
+    document.getElementById("modalBusca").style.display = 'block';
+  }
   function HideandCleanModal() {
     document.getElementById("modal").style.display = 'none';
     document.getElementById("forma").value = ""
@@ -40,14 +43,14 @@ function RpgDemonSlayer() {
     document.getElementById("respiracao").value = ""
     document.getElementById("descricao").value = ""
   }
-
   function showError() {
     document.getElementById("error").style.display = 'block';
   }
   function hideError() {
     document.getElementById("error").style.display = 'none';
+    document.getElementById("errorpesquisa").style.display = 'none';
+    console.log("");
   }
-
   function showSucsess() {
     document.getElementById("sucesso").style.display = 'block';
   }
@@ -74,6 +77,36 @@ function RpgDemonSlayer() {
 
   }
 
+  function handleSearch() {
+    document.getElementById("errorpesquisa").innerHTML = '';
+    const busca = document.getElementById("busca").value;
+
+    if (busca != "") {
+      
+      habilidade.map((item) => {
+        if (item.nome === busca) {
+
+          setHabilidade([{
+            forma: item.forma,
+            nome: item.nome,
+            respiracao: item.respiracao,
+            descricao: item.descricao
+          }])
+
+          document.getElementById("errorpesquisa").innerHTML = '';
+
+        } else {
+          document.getElementById("errorpesquisa").innerHTML = 'Habilidade não encontrada!';
+        }
+      })
+    } else {
+      document.getElementById("errorpesquisa").innerHTML = 'Preencha o campo com a Respiração para fazer a busca!';
+    }
+
+
+  }
+
+
 
   return (
     <div className="App">
@@ -83,6 +116,7 @@ function RpgDemonSlayer() {
       </header>
       <div className="forms">
         <div className="linhas">
+          <p className='addRespira' onClick={showModalBusca} > Buscar Respiração </p>
           <p className='addRespira' onClick={showModal} > Adicionar Respiração </p>
           <p id="sucesso"> Respiração cadastrada com sucesso!</p>
         </div>
@@ -93,11 +127,20 @@ function RpgDemonSlayer() {
             <input type="text" id="nome" required className="input" placeholder='Nome' />
             <input type="text" id="respiracao" required className="input" placeholder='Classe' />
             <input type="text" id="descricao" required className="input" placeholder='Descrição' />
-
-
             <div className="linha">
               <p className="button" onClick={handleAdd}>Adicionar</p>
               <p id="error"> Preencha todos os Campos!</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="box-busca" id='modalBusca'>
+          <div className="input-group">
+            <input type="text" id="busca" required className="input" placeholder='Digite uma magia para Buscar!' />
+
+            <div className="linha">
+              <p className="button" onClick={handleSearch}>Buscar</p>
+              <p id="errorpesquisa"></p>
             </div>
 
 
@@ -106,10 +149,10 @@ function RpgDemonSlayer() {
         </div>
       </div>
 
-      <div className='main'>
+      <div className='main' >
         {habilidade.map((habilidade) => {
           return (
-            <div className='box-hability'>
+            <div className='box-hability' key={habilidade.nome}>
               <ul className="ulRpg" id={habilidade.nome}>
                 <li className='subtitle'> {habilidade.forma} - <b>{habilidade.nome}</b></li>
                 <li className='classe'>{habilidade.respiracao}</li>
@@ -119,6 +162,7 @@ function RpgDemonSlayer() {
           );
         })}
       </div>
+
 
       <footer>
         <img src={muzan} className='muzan' />
